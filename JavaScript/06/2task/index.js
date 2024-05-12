085672ce7404515cd8b0d229e79ae03e103699a2
Alexandr <alexandralexv@gmail.com>
2024-05-12 15:01:07 +0300

    06/1task

---
+++ JavaScript/06/2task/index.js
@@ -0,0 +1,9 @@
+let arr = ['arr', 'rar', 'winRaR'];
+
+arr.pop(); // удаляет с конца
+arr.push('table'); // добавляет в конец
+
+arr.shift(); // удаляет с начала
+arr.unshift('lin') // добавляет в начало
+
+console.log(arr);
